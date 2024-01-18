77816902051fe9f5ca22ddcdcc2de0b4a26ec823
Joshua <algormado@gmail.com>
2024-01-18 17:12:20 +0300

    feat:javascript update

--- .github/workflows/app.js
+++ .github/workflows/app.js
@@ -1,16 +1,22 @@
 // Sample cat facts 
 document.addEventListener('DOMContentLoaded', function() {
     getRandomCatFact();
+    
   });
 
   function getRandomCatFact(){}
     const catFactElement = document.getElementById('catFact');
     const getFactBtn = document.getElementById('getFactBtn');
+    const colorChangeButton = document.getElementById('colorChangeButton');
 
+    colorChangeButton.addEventListener('click', function () {
+      // Toggle the 'clicked' class to change the button color
+      colorChangeButton.classList.toggle('clicked');
+    });
     // Disable the button while fetching a new fact
     getFactBtn.disabled = true;
 
-    // Fetching data from a public Cat Facts API (replace with your desired API)
+    // Fetching data from a public Cat Facts API 
     fetch('https://meowfacts.herokuapp.com/')
       .then(response => response.json())
       .then(data => {
